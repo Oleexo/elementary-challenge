ecf8f911ffb6534588a23d4bb2841c0d36fd8255
Maxime Charles <maxfoow@gmail.com>
2017-07-03 13:55:58 +0200

    first try for 2 first chall

--- index.js
+++ index.js
@@ -22,7 +22,7 @@ if (new Date() > endDate) {
     });
     app.post('/45be5cbf-e947-41be-8d9f-e59f4790ea5a', function (req, res) {
         /* challenge 1 */
-        if (req.body.answer.toUpperCase() == "01".toUpperCase()) {
+        if (req.body.answer.toUpperCase() == "Follow your dreams".toUpperCase()) {
             res.send("de75400a-f977-4dc6-af1b-e5f1f105a4b5")
         } else {
             res.send(false)
@@ -31,7 +31,7 @@ if (new Date() > endDate) {
 
     app.post('/2be2ec2a-98fe-4791-902b-fde0f14a76d5', function (req, res) {
         /* challenge 2 */
-        if (req.body.answer.toUpperCase() == "02".toUpperCase()) {
+        if (req.body.answer.toUpperCase() == "Vi Veri universum vivus vici".toUpperCase()) {
             res.send("37ac2ab2-c16b-4996-9db3-b5611a55c0f9")
         } else {
             res.send(false)
